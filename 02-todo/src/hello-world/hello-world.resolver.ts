62b81b580371fc75d9d2371349619c1edfe02c73
Luis Cuya <luis.cuya@intelica.com>
2025-05-17 13:03:50 -0500

    Endpoints with parameter

--- 02-todo/src/hello-world/hello-world.resolver.ts
+++ 02-todo/src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,23 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(() => String)
+    @Query(() => String, { description: 'Hola mundo es lo que retorna', name: 'hello' })
     helloWorld(): string {
         return 'Hello World!';
+    }
+
+    @Query(() => Float, { name: 'random' })
+    getRandomNumber(): number {
+        return Math.floor(Math.random() * 100);
+    }
+
+    //with parameters
+    @Query(() => Int, { name: 'randomFromZeroTo', description: 'Tendra un valor por defecto 6' })
+    getRandomFromZeroTo(
+        @Args('parameter1', { nullable: true, type: () => Int  }) parameter1 : number = 6
+    ): number {
+        return Math.floor(Math.random() * parameter1);
     }
 }
