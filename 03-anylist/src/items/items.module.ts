28e08cb57f7bab50f0adbaefc6b2732973d0290a
Luis Cuya <luis.cuya@intelica.com>
2025-05-18 13:34:36 -0500

    Creacion de item entity (tabla de postgresql) y configuracion en el module

--- 03-anylist/src/items/items.module.ts
+++ 03-anylist/src/items/items.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemsResolver } from './items.resolver';
+import { Type } from 'class-transformer';
+import { Item } from './entities/item.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [ItemsResolver, ItemsService],
+  imports: [
+    TypeOrmModule.forFeature([Item]) // Import the Item entity
+  ],
 })
 export class ItemsModule {}
