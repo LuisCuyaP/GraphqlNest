5472c212220484df4ca52e5972b7c8d45a2ec9d7
Luis Cuya <luis.cuya@intelica.com>
2025-05-19 16:21:32 -0500

    Configuration JWT en nest

--- 03-anylist/src/auth/auth.module.ts
+++ 03-anylist/src/auth/auth.module.ts
@@ -2,11 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthResolver, AuthService],
   imports: [
-    UsersModule
+    ConfigModule, // Import ConfigModule for environment variables
+    PassportModule.register({ defaultStrategy: 'jwt' }), // Import PassportModule with JWT strategy
+    JwtModule.registerAsync({
+      imports: [ConfigModule], // Import ConfigModule to access environment variables
+      inject: [ConfigService], // Inject ConfigService to use in the factory function
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'), // Get JWT secret from environment variables
+        signOptions: {
+          expiresIn: '4h', // Token expiration time
+        }
+      })
+    }), // Register JwtModule asynchronously
+    UsersModule,
   ],
 })
 export class AuthModule {}
