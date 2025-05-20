a5b83215bbea9477189887cbe12bcde89e14ecb5
Luis Cuya <luis.cuya@intelica.com>
2025-05-20 16:25:46 -0500

    feat: add buscar user por id

--- 03-anylist/src/users/users.resolver.ts
+++ 03-anylist/src/users/users.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.args';
-import { UseGuards } from '@nestjs/common';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
@@ -22,8 +22,9 @@ export class UsersResolver {
 
   @Query(() => User, { name: 'user' })
   findOne(
-    @Args('id', { type: () => ID }) id: string): Promise<User> {
-    return this.usersService.findOneByEmail(id);
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User): Promise<User> {
+    return this.usersService.findOneById(id);
   }
 
   @Mutation(() => User)
