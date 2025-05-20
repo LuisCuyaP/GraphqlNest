a686efc15b87cf5dfdd3b50806be798af1bd1212
Luis Cuya <luis.cuya@intelica.com>
2025-05-20 15:05:55 -0500

    feat: add Args personalizados (parametros de entrada del endpoint)

---
+++ 03-anylist/src/users/dto/args/roles.args.ts
@@ -0,0 +1,10 @@
+import { ArgsType, Field } from "@nestjs/graphql";
+import { IsArray } from "class-validator";
+
+@ArgsType()
+export class ValidRolesArgs {
+
+    @Field(() => [String], { nullable: true })
+    @IsArray()
+    roles: string[] = [];
+}
--- 03-anylist/src/users/users.resolver.ts
+++ 03-anylist/src/users/users.resolver.ts
@@ -1,25 +1,24 @@
 import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { ValidRolesArgs } from './dto/args/roles.args';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  findAll(): Promise<User[]> {
+  findAll(
+    @Args() validRoles: ValidRolesArgs
+  ): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  findOne(
+    @Args('id', { type: () => ID }) id: string): Promise<User> {
     return this.usersService.findOneByEmail(id);
   }
-
-/*   @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
-  } */
 
   @Mutation(() => User)
   blockUser(@Args('id', { type: () => ID }) id: string): Promise<User> {
