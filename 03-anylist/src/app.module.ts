f3d71d5327423fc5aa2abfe528b08adc77197f12
Luis Cuya <luis.cuya@intelica.com>
2025-05-20 21:23:36 -0500

    Bloquear graphql schema con graphqlmodule forRootAsync

--- 03-anylist/src/app.module.ts
+++ 03-anylist/src/app.module.ts
@@ -8,19 +8,40 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
 
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      imports: [AuthModule],
+      inject: [JwtService],
+      useFactory: async(jwtService: JwtService) => ({
+          playground: false,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          plugins: [
+            ApolloServerPluginLandingPageLocalDefault()
+          ],
+          context({req}) {
+            //const token = req.headers.authorization?.replace('Bearer ', '');
+            //if(!token) throw Error('Token needed');
+
+            //const payload = jwtService.decode(token);
+            //if(!payload) throw Error('Token not valid');
+          }
+      })
+    }),
+
+/*     GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),      
       plugins: [
         ApolloServerPluginLandingPageLocalDefault()
       ]
-    }),
+    }), */
 
     TypeOrmModule.forRoot({
       type: 'postgres',
