8b76afbeaaaec53d8abdaf5d95c6a58376c75889
Luis Cuya <luis.cuya@intelica.com>
2025-05-23 10:44:14 -0500

    feat: sedd part II - proteger para no eliminar data de prd

--- 03-anylist/src/seed/seed.service.ts
+++ 03-anylist/src/seed/seed.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
 
-    constructor(){}
+    private isProd: boolean;
 
-    async executeSeed(): Promise<boolean> {
+    constructor(
+        private readonly configService: ConfigService
+    ){
+        this.isProd = this.configService.get('STATE') === 'prod';
+    }
+
+    async executeSeed() {
+        if(this.isProd)
+            throw new UnauthorizedException('No se puede ejecutar el seed en produccion');
+        
+        
+        
+        
         return true;
     }
 }
